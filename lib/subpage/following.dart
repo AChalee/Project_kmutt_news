04e7f2321acb1218af493c55f1b753b99bb144e2
AChalee <64572634+AChalee@users.noreply.github.com>
2020-05-12 19:45:22 +0700

    update

--- lib/subpage/following.dart
+++ lib/subpage/following.dart
@@ -19,6 +19,33 @@ class _FollowingState extends State<Following> {
           style: TextStyle(fontSize: 24, color: Colors.white),
         ),
       ),
+      body: CustomScrollView(
+  primary: false,
+  slivers: <Widget>[
+    SliverPadding(
+      padding: const EdgeInsets.all(20),
+      sliver: SliverGrid.count(
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text("page"),
+            color: Colors.orange[100],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[200],
+          ),
+          
+        ],
+      ),
+    ),
+  ],
+)
+  
     );
   }
 }
