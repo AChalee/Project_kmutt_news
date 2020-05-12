0913ffb89b149cb4d234675252a66f35e9177af2
AChalee <64572634+AChalee@users.noreply.github.com>
2020-05-12 20:45:26 +0700

    saveall

--- lib/mainpage/follow.dart
+++ lib/mainpage/follow.dart
@@ -19,6 +19,60 @@ class _FollowState extends State<Follow> {
           style: TextStyle(fontSize: 24, color: Colors.white),
         ),
       ),
+      body: CustomScrollView(
+  primary: false,
+  slivers: <Widget>[
+    SliverPadding(
+      padding: const EdgeInsets.all(20),
+      sliver: SliverGrid.count(
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text("page"),
+            color: Colors.orange[100],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[200],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[300],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[400],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[500],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[700],
+          ),Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('page'),
+            color: Colors.orange[800],
+          ),
+        ],
+      ),
+    ),
+  ],
+)
     );
   }
 }
--- lib/mainpage/more.dart
+++ lib/mainpage/more.dart
@@ -20,33 +20,66 @@ class _AddMoreState extends State<AddMore> {
         ),
 
       ),
-body: Column(
-  children: <Widget>[
-        ListTile(
-      title: Text('คณะ',
-        style: TextStyle(
-          fontSize: 18,
-          color: Colors.black54
-        ),),
+body:  ListView.builder(
+          itemBuilder: (BuildContext context, int index) =>
+              EntryItem(data[index]),
+          itemCount: data.length,
+        ),
+      );
+    
+  }
+}
+class Entry{
+  Entry(this.title,[this.children = const <Entry>[]]);
 
-    ),
-    ListTile(
-      title: Text('สาขา/ภาควิชา',
-      style: TextStyle(
-        fontSize: 18,
-        color: Colors.black45
-      ),),
-    ),
-    ListTile(
-      title: Text('ชั้นปี',
-        style: TextStyle(
-            fontSize: 18,
-            color: Colors.black45
-        ),),
-    ),
+  final String title;
+  final List<Entry> children;
+}
+
+final List<Entry> data = <Entry>[
+  Entry(
+    'คณะ',
+    <Entry>[
+      Entry('วิศวกรรมศาสตร์',
+      <Entry>[
+        Entry('วิศวกรรมเครื่องกล'),
+      ],
+      ),
+      Entry('วิทยาศาสตร์'),
+      Entry('ครุศาสตร์อุุตสาหกรรมและเทคโนดลยี'),
+      Entry('โครงการร่วมบริหารหลักสูตรมีเดียอาร์ตและเทคโนโลยี'),
+      Entry('เทคดนโลยีสารสนเทศ'),
+      Entry('สถาปัตยกรรมศาสตร์และการออกแบบ'),
+      Entry('สถาบันวิทยาการหุ่นยนต์ภาคสนาม'),
+      Entry('วิทยาลัยสหวิทยาการ'),
+    ],
+  ),
+  Entry(
+    'ชั้นปี'
+  ),
+Entry(
+  'อื่นๆ',
+  <Entry>[
+    Entry('ปริญญาโท'),
+    Entry('ปริญญาเอก'),
   ],
 ),
+];
 
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title : Text(root.title));
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildTiles).toList(),      
     );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
   }
 }
